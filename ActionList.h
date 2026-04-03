4f2e24d8029a2a00847c71672c9fdec67681e4a9
jodyselyman321-lang <jodyselyman321@gmail.com>
2026-04-03 03:32:09 +0200

    Add ActionList class with action management methods

---
+++ ActionList.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Queue.h"
+#include "Action.h"
+#include "Order.h"
+
+class Restaurant; // Forward declaration
+
+class ActionList {
+private:
+    Queue<Action*> actionsQueue;
+
+public:
+    ActionList();
+    ~ActionList();
+    
+    void AddAction(Action* pAct);
+    void ExecuteActions(int currentTime, Restaurant* pRest);
+};
+
+// --- Specific Action Classes ---
+
+class RequestAction : public Action {
+    Order* pOrd;
+public:
+    RequestAction(int time, Order* o);
+    virtual void Execute(Restaurant* pRest);
+};
+
+class CancelAction : public Action {
+    int orderID;
+public:
+    CancelAction(int time, int id);
+    virtual void Execute(Restaurant* pRest);
+};
