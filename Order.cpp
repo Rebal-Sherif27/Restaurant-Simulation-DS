6b0566af4a0dd12b107cfca0078cf899ed6bbd7a
jodyselyman321-lang <jodyselyman321@gmail.com>
2026-04-07 22:59:55 +0200

    Implement Order class constructor and destructor

---
+++ Order.cpp
@@ -0,0 +1,15 @@
+#include "Order.h"
+
+Order::Order(int id, OrderType t, int size, int price, int reqTime) {
+    this->id = id;
+    this->type = t;
+    this->size = size;
+    this->price = price;
+    this->requestTime = reqTime;
+    seats = 0;
+    duration = 0;
+    canShare = false;
+    distance = 0;
+}
+
+Order::~Order() {}
