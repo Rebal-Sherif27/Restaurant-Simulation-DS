3f41ffac45ab27b2d9b418b773188ab4d79c3bb9
jodyselyman321-lang <jodyselyman321@gmail.com>
2026-04-07 23:14:22 +0200

    Define OrderType enum and Order class
    Added OrderType enum and Order class definition.

--- RestaurantSim/order.h
+++ RestaurantSim/order.h
@@ -22,4 +22,36 @@ public:
 	void assignOrder(Order* newOrder);
 	void freeTable();
 };
+////jody
+
+
+enum OrderType {
+    ODG,  // dine-in grilled
+    ODN,  // dine-in normal
+    OT,   // takeaway
+    OVG,  // delivery grilled
+    OVC,  // delivery cold
+    OVN   // delivery normal
+};
+
+class Order {
+public:
+    int id;
+    OrderType type;
+    int size;
+    int price;
+    int requestTime;
+
+    // For dine-in only
+    int seats;
+    int duration;
+    bool canShare;
+
+    // For delivery only
+    int distance;
+
+    Order(int id, OrderType t, int size, int price, int reqTime);
+    ~Order();
+};
+
 
