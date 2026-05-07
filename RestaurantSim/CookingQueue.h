2e48c22eb349b08b643031fa61fa06157fba9ec2
Shahd <shahd.sawi06@eng-st.cu.edu.eg>
2026-05-07 07:09:15 +0300

    Fix cooking queue cancel removal

--- RestaurantSim/CookingQueue.h
+++ RestaurantSim/CookingQueue.h
@@ -8,29 +8,41 @@ class CookingQueue : public priQueue<Order*>
 {
 public:
 
-	bool CancelOrder(int cancelID)
-	{
-		if (this->isEmpty()) {
-			return false;
-		}
-		int dummyPri;
-		if (head->getItem(dummyPri)->id == cancelID)
-		{
-			priNode<Order*>* temp = head;
-			head = head->getNext();
-			delete temp;
-			return true;
-		}
-		priNode<Order*>* current = head;
-		while (current->getNext() != nullptr) {
-			if (current->getNext()->getItem(dummyPri)->id == cancelID) {
-				priNode<Order*>* nodeToDelete = current->getNext();
-				delete nodeToDelete;
-				return true;
-			}
-			current = current->getNext();
-		}
-		return false;
-	}
+    bool CancelOrder(int cancelID)
+    {
+        if (this->isEmpty())
+        {
+            return false;
+        }
+
+        int dummyPri;
+
+        if (head->getItem(dummyPri)->id == cancelID)
+        {
+            priNode<Order*>* temp = head;
+            head = head->getNext();
+            delete temp;
+            count--;
+            return true;
+        }
+
+        priNode<Order*>* current = head;
+
+        while (current->getNext() != nullptr)
+        {
+            if (current->getNext()->getItem(dummyPri)->id == cancelID)
+            {
+                priNode<Order*>* nodeToDelete = current->getNext();
+                current->setNext(nodeToDelete->getNext());
+                delete nodeToDelete;
+                count--;
+                return true;
+            }
+
+            current = current->getNext();
+        }
+
+        return false;
+    }
 };
 #endif
